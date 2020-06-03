398c435c0555684208c216f1ae72f75505fcfe5e
AFeese <58886725+AFeese@users.noreply.github.com>
2020-06-03 12:17:15 -0400

    Created burger table and insertions for different types.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Blue Boi');
+INSERT INTO burgers (burger_name) VALUES ('Spinach Tomato');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast Stack', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Caramel Bacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tuna Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Classic');
