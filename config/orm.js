356c44fa23d0894a4a8d9268fc20b0ba5d4ceb69
AFeese <58886725+AFeese@users.noreply.github.com>
2020-06-03 13:12:24 -0400

    added directory set up. Ready to test temp files and make adjustments where needed.

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-const connection = require("..config/connection.js");
+const connection = require("../config/connection.js");
 
 //---------------NOT SURE IF WORKS YET--------------//
 //--------------------------------------------------//
